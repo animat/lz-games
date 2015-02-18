54f6b608c5abae54843e6f63f3e077126af67542
Ivanixcu <ivanixcu@IvanixMacMini.local>
2015-02-18 21:09:06 +0800

    branch: prep2a
    desc:
    - Fix ownership command in prep.sh
    - Add "gulp build_all" command to prep.sh
    - Fix missing svr/game.xml (replaced symlink with data file)
    - Removed html files that should be recreated using "gulp inc"
    
    notes:
    after checkout from git,
    run command "bash prep.sh"  from html5_games directory
    to recreate all html files and compile games into build directory

--- html5_games/prep.sh
+++ html5_games/prep.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo chown -R  $USER:$USER .
+gid=`id -g`
+
+echo "$0:  Changing ownership to $USER:$gid"
+sudo chown -R  $USER:$gid .
+echo "$0: Setting folder permisisons"
 find  sources -type d -exec chmod 755  {} \;
+echo "$0: Setting file permisisons"
 find  sources -type f -exec chmod 644  {} \;
+echo "$0: Installing npm modules"
 npm install
+gulp build_all
