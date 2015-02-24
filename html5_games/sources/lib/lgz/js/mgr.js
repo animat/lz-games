8d2071e48edabc0cae8012f82393ded9d969dc33
ivanixgames <ivanixcu@IvanixVIAO>
2015-02-24 18:25:41 +0800

    Branch: libaudio1a Desc: - Fix audio for ios - Add K.volumeLevel for Lgz.mgr.sfx

--- html5_games/sources/lib/lgz/js/mgr.js
+++ html5_games/sources/lib/lgz/js/mgr.js
@@ -203,9 +203,9 @@ LgzLib.Mgr = function (globLgz, gamePrefix) {
         g.json = loader.json;
         loader.audio = thisObj.game.load.audio(
             name,
-            K.urlRscMedia + name + '.ogg',
+            [K.urlRscMedia + name + '.ogg',
             K.urlRscMedia + name + '.mp3',
-            K.urlRscMedia + name + '.m4a'
+            K.urlRscMedia + name + '.m4a']
         );
         return loader;
     };
@@ -226,7 +226,7 @@ LgzLib.Mgr = function (globLgz, gamePrefix) {
         return sfx;
     };
     thisObj._soundPlay = function (key, cutoff) {
-        thisObj.sfx.play(key);
+        thisObj.sfx.play(key, 0, K.volumeLevel);
         if (cutoff) {
             window.setTimeout(
                 function () {
