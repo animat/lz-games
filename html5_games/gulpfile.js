61387923873ee357e4ca7479d5e30d3acc70842c
ivanixgames <ivanixcu@IvanixVIAO>
2015-05-24 17:08:54 +0800

    branch: gulpfile_min1a desc: Followup build code to provide more robust building of minified games. Commands as follows:
    "gulp compile_game" (run inside game directory)
          Creates (game)/min folder with minified code files.
    
    "gulp build_game" (run inside game directory)
          Creates build/min/(game) folder with minified code files
          Creates build/raw/(game) folder with uncompressed code files
    
    "gulp build_lib"
          Creates build/min/lib folder stripped of css/js
          Creates build/raw/lib folder with uncompressed code files
    
    "gulp build_svr"
          Creates build/min/svr folder
          Creates build/raw/svr folder
    
    "gulp build_all"
          Runs "build_lib", "build_svr" and "build_game" for every game in
          sources folder.

--- html5_games/gulpfile.js
+++ html5_games/gulpfile.js
@@ -1,4 +1,10 @@
-// include gulp
+/*global
+    $: true,
+    require: true,
+    process: true,
+    console: true
+ */
+/*jslint  nomen: true */
 
 var path = require('path');
 var gulp = require('gulp');
@@ -12,13 +18,18 @@ var open = require('open');
 var util = require('util');
 var runSequence = require('run-sequence');
 
-// include plug-ins
-var minifyHTML = require('gulp-minify-html');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglifyjs');
+var replace = require('gulp-replace');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
+
  
 // include plug-ins
 var concat = require('gulp-concat');
 var stripDebug = require('gulp-strip-debug');
-var uglify = require('gulp-uglify');
 
 var shell =  require('gulp-shell');
 
@@ -26,69 +37,28 @@ var shell =  require('gulp-shell');
 // don't rely on __dirname 
 // either gulp or nodejs ignores symbolic links :(
 
+var rootDir = process.cwd();
+console.info('rootDir: ' + rootDir);
 
 var gameDir = path.basename(process.env.INIT_CWD);
 console.info('gameDir: ' + gameDir);
 
-var buildDir = '../../build/'  + gameDir;
-console.info('buildDir: ' + buildDir);
+var buildRaw = rootDir + '/build/raw/' + gameDir;
+console.info('buildRaw: ' + buildRaw);
+
+var buildMin = rootDir + '/build/min/' + gameDir;
+console.info('buildMin: ' + buildMin);
 
 
-var tailDir = path.basename(process.env.INIT_CWD);
-// console.info('gulpfile.js: INIT_CWD: ' + process.env.INIT_CWD);
-console.info('gulpfile.js: tailDir: ' + tailDir);
+var tailDir = path.dirname(process.env.INIT_CWD);
+console.info('tailDir: ' + tailDir);
 
-
-var gulpDir = process.cwd();
-// console.info('gulpfile.js: gulpDir: ' + gulpDir);
 
 // Added to allow calling gulpfile.js from game subdirectory
 // and avoid having symbolic link back up to root directory (html5_games)
 process.chdir(process.env.INIT_CWD);
-console.info('gulpfile.js: Working directory now: ' + process.cwd());
+//console.info('CWD: ' + process.cwd());
 
-
-gulp.task('nop', function () {
-	'use strict';
-});
-
-//JS hint task
-gulp.task('jshint', function () {
-	'use strict';
-  
-	gulp.src('./rsc/js/*.js').pipe(jshint()).pipe(jshint.reporter('default'));
-});
-//Watch files changes during dev/debug
-gulp.task('watch', function () {
-	'use strict';
-	watch(['lib/exodom/exo.html', 'lib/lgz/dom/*.html', 'rsc/dom/*.html', './*.inc'], function () {
-		gulp.start('inc');
-		// open('http://localhost/Projects/Clients/LinguaZone/lz-games/html5_games/sources/Splat/game.html');
-	});
-	
-});
-
-
-// not used:  minify new images
-gulp.task('xxx_imagemin', function () {
-	'use strict';
-	var src, dst;
-
-	//src = './rsc/img/*.png';
-	src = './rsc/img/*';
-	dst = buildDir + '/rsc/img';
-	//gulp.src(src).pipe(changed(dst)).pipe(imagemin()).pipe(gulp.dest(dst));
-	gulp.src(src).pipe(imagemin()).pipe(gulp.dest(dst));
-});
-// not used:  minify and copy lib assets
-gulp.task('xxx_build_minlib', function () {
-	'use strict';
-	var dst, srcArr;
-	srcArr = ['lib/js/*.js', '!lib/js/*.min.js', 'lib/lgz/*.js' ];
-	gulp.src(srcArr).pipe(concat('lib.js')).pipe(uglify()).pipe(gulp.dest(buildDir));
-	gulp.src('lib/js/*.min.js').pipe(gulp.dest(buildDir + '/lib/js'));
-	gulp.src('lib/lgz/*.png').pipe(imagemin()).pipe(gulp.dest(buildDir + '/lib/lgz'));
-});
 
 
 gulp.task('inc', function () {
@@ -101,94 +71,17 @@ gulp.task('inc', function () {
 		.pipe(gulp.dest('./'));
 	
 });
-gulp.task('compile_game', ['inc'], function () {
+
+gulp.task('min_clean',  function () {
 	'use strict';
-
-});
-
-gulp.task('build_subsvr', function () {
-	'use strict';
-	var dst, srcArr;
-	srcArr = ['*'];
-	gulp.src(srcArr).pipe(gulp.dest(buildDir));
-
-});
-
-
-gulp.task('build_svr', function () {
-    'use strict';
-    var srcArr;
-
-	srcArr = ['./sources/svr'];
-	return gulp.src(srcArr, {read: false})
-        .pipe(shell(
-	        [
-                'echo cleaning build of:  <%= f(file.path) %>',
-                'cd <%= f(file.path) %>; gulp build_clean',
-                'echo creating build of:  <%= f(file.path) %>',
-                'cd <%= f(file.path) %>; gulp build_subsvr'
-	        ],
-            {
-                templateData: {
-                    f: function (s) {
-                        return s;
-                    }
-                }
-            }
-        ));
-});
-gulp.task('build_sublib', function () {
-	'use strict';
-	var dst, srcArr;
-
-	gulp.src('exodom/**/*').pipe(gulp.dest(buildDir + '/exodom'));
-
-	gulp.src('vendor/**/*').pipe(gulp.dest(buildDir + '/vendor'));
-
-	//gulp.src('vendor/phaser/*').pipe(gulp.dest(buildDir + '/vendor/phaser'));
-	//gulp.src('vendor/phaser/plugins/*').pipe(gulp.dest(buildDir + '/vendor/phaser/plugins'));
-
-	srcArr = ['lgz/mma/*', '!lgz/mma/*.svg'  ];
-	gulp.src(srcArr).pipe(gulp.dest(buildDir + '/lgz/mma'));
-
-	srcArr = ['lgz/js/*'];
-	gulp.src(srcArr).pipe(gulp.dest(buildDir + '/lgz/js'));
-
-	srcArr = ['lgz/dom/*.css'];
-	gulp.src(srcArr).pipe(gulp.dest(buildDir + '/lgz/dom'));
-
-});
-gulp.task('build_lib', function () {
-    'use strict';
-    var srcArr;
-
-	srcArr = ['./sources/lib'];
-	return gulp.src(srcArr, {read: false})
-        .pipe(shell(
-	        [
-                'echo cleaning build of:  <%= f(file.path) %>',
-                'cd <%= f(file.path) %>; gulp build_clean',
-                'echo creating build of:  <%= f(file.path) %>',
-                'cd <%= f(file.path) %>; gulp build_sublib'
-	        ],
-            {
-                templateData: {
-                    f: function (s) {
-                        return s;
-                    }
-                }
-            }
-        ));
-});
-gulp.task('build_clean', function () {
-    'use strict';
     var dst;
-	dst = [buildDir];
+	dst = './';
 	return gulp.src(dst, {read: false})
         .pipe(shell(
 	        [
-                'echo cleaning:  <%= f(file.path) %>',
-                'rm -rf  "<%= f(file.path) %>"'
+                'echo "cleaning `pwd`/min" ',
+                'rm -f min/*;'
+
 	        ],
             {
                 templateData: {
@@ -199,15 +92,16 @@ gulp.task('build_clean', function () {
             }
         ));
 });
-gulp.task('build_links',  function () {
+gulp.task('min_links',  function () {
 	'use strict';
     var dst;
-	dst = [buildDir];
+	dst = 'min/';
 	return gulp.src(dst, {read: false})
         .pipe(shell(
 	        [
-                'echo "linking lib, svr in pwd: `pwd` with path: <%= f(file.path) %>!" ',
-                'cd <%= f(file.path) %>; ln -s ../lib lib; ln -s ../svr svr;'
+                'echo "copying lib/svr links to ./min" ',
+                'cp -a lib <%= f(file.path) %>/.;',
+                'cp -a svr <%= f(file.path) %>/.;'
 	        ],
             {
                 templateData: {
@@ -218,68 +112,188 @@ gulp.task('build_links',  function () {
             }
         ));
 });
-gulp.task('build_html',  function () {
+gulp.task('min_compile', function () {
+    'use strict';
+    return gulp.src('./game.html')
+        .pipe(replace(/css\?/g, 'css'))
+        .pipe(replace(/js\?/g, 'js'))
+        .pipe(usemin({
+            css: [minifyCss(), 'concat'],
+            html: [minifyHtml({empty: true})],
+            js1: [uglify({
+                outSourceMap: true,
+                compress: {drop_console: true}
+            })],
+            js2: [uglify({
+                outSourceMap: true,
+                compress: {drop_console: true}
+            })],
+            js3: [uglify({
+                outSourceMap: true,
+                compress: {drop_console: true}
+            })]
+        }))
+        .pipe(gulp.dest('min/'));
+});
+
+gulp.task('compile_game',  function (cb) {
+	'use strict';
+    runSequence(
+        'inc',
+        'min_clean',
+        'min_compile',
+        'min_links',
+        cb
+    );
+
+});
+
+gulp.task('build_raw_clean', shell.task([
+    'echo cleaning ' + buildRaw,
+    'mkdir  -p ' + buildRaw,
+    'rm -rf ' + buildRaw + '/*'
+ 
+
+]));
+gulp.task('build_min_clean', shell.task([
+    
+    'echo cleaning ' + buildMin,
+    'rm -rf ' + buildMin,
+    'mkdir  -p ' + buildMin
+
+]));
+gulp.task('build_raw_svr', function () {
+	'use strict';
+	var  srcArr;
+	srcArr = ['**/*'];
+	return gulp.src(srcArr).pipe(gulp.dest(buildRaw));
+
+});
+gulp.task('build_min_svr', function () {
+	'use strict';
+	var  srcArr;
+	srcArr = ['**/*'];
+	return gulp.src(srcArr).pipe(gulp.dest(buildMin));
+
+});
+ 
+
+gulp.task('build_svr', function (cb) {
+    'use strict';
+    var srcDir;
+    srcDir = rootDir + '/sources/svr';
+    process.chdir(srcDir);
+    runSequence(
+        'build_raw_clean',
+        'build_raw_svr',
+        'build_min_clean',
+        'build_min_svr',
+        cb
+    );
+
+});
+gulp.task('build_raw_lib', function () {
+	'use strict';
+	var srcArr;
+	srcArr = ['**/*', '!**/*.html'];
+	gulp.src(srcArr).pipe(gulp.dest(buildRaw));
+
+});
+gulp.task('build_min_lib', function () {
+	'use strict';
+	var srcArr;
+	srcArr = ['**/*', '!**/*.js', '!**/*.svg', '!**/*.css', '!**/*.html'];
+	gulp.src(srcArr).pipe(gulp.dest(buildMin));
+
+});
+gulp.task('build_lib', function (cb) {
+    'use strict';
+    var srcDir;
+    srcDir = rootDir + '/sources/lib';
+    process.chdir(srcDir);
+    runSequence(
+        'build_raw_clean',
+        'build_raw_lib',
+        'build_min_clean',
+        'build_min_lib',
+        cb
+    );
+
+});
+gulp.task('build_raw_links',  function () {
+	'use strict';
+    var dst;
+	dst = ['svr','lib'];
+	return gulp.src(dst, {read: false})
+        .pipe(shell(
+	        [
+                'echo "cp -a <%= f(file.path) %> ' + buildRaw + '/. "',
+                'cp -a <%= f(file.path) %> ' +  buildRaw + '/. '
+	        ],
+            {
+                templateData: {
+                    f: function (s) {
+                        return s;
+                    }
+                }
+            }
+        ));
+});
+gulp.task('build_raw_html',  function () {
 	'use strict';
     var srcArr;
 
 	srcArr = ['*.html'];
     console.log('building html files');
-	return gulp.src(srcArr).pipe(gulp.dest(buildDir));
-
-
+	return gulp.src(srcArr).pipe(gulp.dest(buildRaw));
 });
-gulp.task('build_css', function () {
+gulp.task('build_raw_css', function () {
 	'use strict';
     var srcArr, rtc;
 
     console.log('building css files');
-	rtc = gulp.src('rsc/dom/*.css').pipe(gulp.dest(buildDir + '/rsc/dom'));
+	rtc = gulp.src('rsc/dom/*.css').pipe(gulp.dest(buildRaw + '/rsc/dom'));
     return rtc;
 
 
 });
-gulp.task('build_js', function () {
+gulp.task('build_raw_js', function () {
 	'use strict';
     var srcArr, rtc;
 
     console.log('building js files');
-	rtc = gulp.src('rsc/js/*').pipe(gulp.dest(buildDir + '/rsc/js'));
+	rtc = gulp.src('rsc/js/*').pipe(gulp.dest(buildRaw + '/rsc/js'));
     return rtc;
 
 });
-gulp.task('build_mma', function () {
+gulp.task('build_raw_mma', function () {
 	'use strict';
     var srcArr, rtc;
 
     console.log('building mma files');
 	srcArr = ['rsc/mma/*', '!rsc/mma/*.svg'  ];
-    rtc = gulp.src(srcArr).pipe(gulp.dest(buildDir + '/rsc/mma'));
+    rtc = gulp.src(srcArr).pipe(gulp.dest(buildRaw + '/rsc/mma'));
     return rtc;
 
 });
-gulp.task('build_game', ['build_clean', 'compile_game'], function () {
+gulp.task('build_min_min', function () {
 	'use strict';
+    var srcArr, rtc;
 
-    runSequence(
-        'build_html',
-        'build_js',
-        'build_css',
-        'build_mma',
-        'build_links'
-    );
-
+    console.log('building min files in ' + buildMin);
+	srcArr = ['min/*.html', 'min/*.js', 'min/*.css', '!min/*.map'  ];
+    rtc = gulp.src(srcArr).pipe(gulp.dest(buildMin));
+    return rtc;
 
 });
-gulp.task('build_games', function () {
-    'use strict';
-    var srcArr;
-
-	srcArr = ['./sources/*', '!./sources/lib', '!./sources/svr'];
-	return gulp.src(srcArr, {read: false})
+gulp.task('build_min_mma',  function () {
+	'use strict';
+	return gulp.src(['rsc/mma'], {read: false})
         .pipe(shell(
 	        [
-                'echo creating build of:  <%= f(file.path) %>',
-                'cd <%= f(file.path) %>; gulp build_game'
+                'echo "copying rsc/mma contents to  <%= f(file.path) %>"',
+                'mkdir -p ' + buildMin + '/rsc',
+                'cp -a <%= f(file.path) %> ' + buildMin + '/rsc/.'
 	        ],
             {
                 templateData: {
@@ -290,7 +304,147 @@ gulp.task('build_games', function () {
             }
         ));
 });
+/*
+gulp.task('build_min_links',  function () {
+	'use strict';
+	return gulp.src(buildMin, {read: false})
+        .pipe(shell(
+	        [
+                'echo "copying lib/svr links to <%= f(file.path) %>" ',
+                'cp -a lib <%= f(file.path) %>/.;',
+                'cp -a svr <%= f(file.path) %>/.;'
+	        ],
+            {
+                templateData: {
+                    f: function (s) {
+                        return s;
+                    }
+                }
+            }
+        ));
+});
+*/
+gulp.task('build_min_links',  function () {
+	'use strict';
+    var dst;
+	dst = ['svr','lib'];
+	return gulp.src(dst, {read: false})
+        .pipe(shell(
+	        [
+                'echo "cp -a <%= f(file.path) %> ' + buildMin + '/. "',
+                'cp -a <%= f(file.path) %> ' +  buildMin + '/. '
+	        ],
+            {
+                templateData: {
+                    f: function (s) {
+                        return s;
+                    }
+                }
+            }
+        ));
+});
+gulp.task('build_min_game', function (cb) {
+	'use strict';
+    runSequence(
+        'build_min_clean',
+        'build_min_min',
+        'build_min_mma',
+        'build_min_links',
+        cb
+    );
+});
+gulp.task('build_raw_game', function (cb) {
+	'use strict';
+    runSequence(
+        'build_raw_clean',
+        'build_raw_html',
+        'build_raw_js',
+        'build_raw_css',
+        'build_raw_mma',
+        'build_raw_links',
+        cb
+    );
 
+});
+gulp.task('build_game1', ['compile_game'], function (cb) {
+	'use strict';
+    runSequence(
+        'build_raw_game',
+        'build_min_game',
+        cb
+    );
 
-gulp.task('default', ['nop'], function () { });
-gulp.task('build_all', ['build_games','build_lib','build_svr'], function () { });
+});
+gulp.task('build_game',['build_game1'], function () {
+	'use strict';
+     process.exit(0);
+});
+gulp.task('build_games', function () {
+    'use strict';
+    var srcArr;
+
+    process.chdir(rootDir);
+
+    srcArr = ['./sources/*', '!./sources/lib', '!./sources/svr'];
+    return gulp.src(srcArr, {read: false})
+    .pipe(shell(
+        [
+            'echo building  <%= f(file.path) %>',
+            'cd <%= f(file.path) %>; gulp build_game'
+        ],
+        {
+            templateData: {
+                f: function (s) {
+                    return s;
+                }
+            }
+        }
+    ));
+});
+gulp.task('build_all', function () {
+	'use strict';
+    runSequence(
+        'build_svr',
+        'build_lib',
+        'build_games'
+    );
+});
+
+gulp.task('build_test2', function () {
+    'use strict';
+    var srcArr, svrDir;
+	return gulp.src('.', {read: false})
+        .pipe( shell(['echo shell: pwd: `pwd`' ])
+        );
+
+});
+gulp.task('build_test1', function () {
+	'use strict';
+    runSequence(
+        'build_test2'
+    );
+    process.exit(0);
+});
+gulp.task('build_test',  function () {
+	'use strict';
+    var dst;
+	dst = ['.'];
+	return gulp.src(dst, {read: false})
+        .pipe(shell(
+	        [
+                'echo "build_test1"',
+                'gulp build_test1 2>&1 '
+	        ],
+            {
+                templateData: {
+                    f: function (s) {
+                        return s;
+                    }
+                }
+            }
+        ));
+});
+gulp.task('default',  function () {
+    'use strict';
+    console.log('no op');
+});
