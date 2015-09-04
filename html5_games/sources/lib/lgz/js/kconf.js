77bcc3ffc4b6cec65d637085914c70afe310ea30
Colin Angevine <info@linguazone.com>
2015-09-03 22:17:05 -0400

    Updated kconf; currently pointing to staging server

--- html5_games/sources/lib/lgz/js/kconf.js
+++ html5_games/sources/lib/lgz/js/kconf.js
@@ -3,38 +3,20 @@ LZCONF.baseUrl = {
     LZPROD: {
         api: 'http://linguazone.com/api/v2',
         games: 'http://linguazone.s3.amazonaws.com/games',
-        lang: 'http://linguazone.com/lang',
-        mma: 'http://linguazone.s3.amazonaws.com/mma'
+        lang: 'http://linguazone.com/games',
+        mma: 'http://linguazone.s3.amazonaws.com'
     },
     LZDEV: {
         api: 'http://localhost:3000/api/v2',
-        games: 'http://linguazone.s3.amazonaws.com/games/raw',
-        lang: 'http://linguazone.com/lang',
-        mma: 'http://linguazone.s3.amazonaws.com/mma'
+        games: 'http://lz-staging.s3.amazonaws.com/games',
+        lang: 'http://lz-staging.herokuapp.com/games',
+        mma: 'http://lz-staging.s3.amazonaws.com'
     },
     LZSTAGING: {
       api: 'http://lz-staging.herokuapp.com/api/v2',
-      games: 'http://lz-staging.s3.amazonaws.com/games/raw',
-      lang: 'http://linguazone.com/lang',
-      mma: 'http://linguazone.s3.amazonaws.com/mma'
-    },
-    IVANIXPUB1: {
-        api: 'http://lgz.ivanix.com/api/v2',
-        games: 'http://lgz.ivanix.com/games',
-        lang: 'http://lgz.ivanix.com/lang',
-        mma: 'http://lgz.ivanix.com/mma'
-    },
-    IVANIXPRIV1: {
-        api: 'http://lgz.ivanix.com/api/v2',
-        games: 'http://192.168.0.10/Projects/Clients/LinguaZone/lz-games/html5_games/sources',
-        lang: 'http://lgz.ivanix.com/lang',
-        mma: 'http://lgz.ivanix.com/mma'
-    },
-    IVANIXPRIV2: {
-        api: 'http://lgz.ivanix.com/api/v2',
-        games: 'http://192.168.2.20/Projects/Clients/LinguaZone/lz-games/html5_games/sources',
-        lang: 'http://lgz.ivanix.com/lang',
-        mma: 'http://lgz.ivanix.com/mma'
+      games: 'http://lz-staging.s3.amazonaws.com/games',
+      lang: 'http://lz-staging.herokuapp.com/games',
+      mma: 'http://lz-staging.s3.amazonaws.com'
     }
 };
 var K =  {};
