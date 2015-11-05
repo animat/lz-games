d157913b2f0387221d193afd6bae7497d430475a
ivanixgames <ivanixcu@IvanixVIAO>
2015-11-06 01:23:43 +0800

    branch: patch_hints_1a desc: adjust parsing of hints nodes in data and handle html formatting.

--- html5_games/sources/lib/lgz/js/hints.js
+++ html5_games/sources/lib/lgz/js/hints.js
@@ -30,21 +30,41 @@ LgzLib.Hints = function (mgr) {
         }
         
     };
-    thisObj.add = function ($winHintAvl, hintnode) {
-        var i,  type,   $btn, $penalty, $para, punit, pval;
-        type = hintnode.getAttribute('type');
+    thisObj.add = function ($winHintAvl, $hintnode) {
+        var i,  type,   $btn, btn, encpenalty, decpenalty, txtpenalty, regval, punit, pval;
+        console.log('LgzLib.Hints.add: $hintnode: ' + $hintnode);
+        type = $hintnode.text();
+        console.log('LgzLib.Hints.add: type: ' + type);
         $btn = $winHintAvl.find('[subref=' + type + ']');
         
         if (!$btn.length) {
-            //todo: log error
+            console.error('LgzLib.Hints.add: no hint button for type: ' + type);
             return;
         }
+        console.log('LgzLib.Hints.add: $btn.length: ' + $btn.length);
 
         $btn.css('display', 'inline');
-        $penalty = $(hintnode).find('penalty');
-        if ($penalty.length) {
-            punit = $penalty.attr('unit');
-            pval =  $penalty.attr('value');
+
+        encpenalty = $hintnode.attr('penalty');
+        decpenalty = decodeURIComponent(encpenalty);
+        console.log('LgzLib.Hints.add: decpenalty: ' + decpenalty);
+
+        if (decpenalty === 'undefined') {
+            return;
+        }
+        $btn[0].innerHTML = decpenalty;
+        txtpenalty = $btn.text();
+
+        regval = txtpenalty.match(/second|point/);
+        if(regval) {
+            punit = regval[0];
+        }
+
+        regval = txtpenalty.match(/[0-9]+/);
+        if(regval) {
+            pval = regval[0];
+        } else {
+            pval = 0;
         }
 
         $btn.click(
@@ -52,30 +72,29 @@ LgzLib.Hints = function (mgr) {
                 thisObj.event(type, punit, pval);
             }
         );
-        if (!$penalty.length) {
-            return;
-        }
-        $para = $btn.find('p')[1];
-        if (!$para) {
-            return;
-        }
 
-        $para.textContent = '+' + pval + ' ' + punit + ' penalty';
+
 
     };
     thisObj.init = function () {
         //note: must be called AFTER nodemgr has loaded xml file
-        var hlist, $winHintAvl, i;
+        var i, hlist, $winHintAvl, $lgzGameDesc, gameDesc;
         
-        hlist = mgr.nm.dataFind('hint');
+        hlist = mgr.nm.dataFind('hints').find('type');
         if (!hlist.length) {
             $('#winHint [subref=none]').css('display','block');
             return;
         }
         $winHintAvl = $('#winHint [subref=avail]');
+
+        gameDesc = mgr.nm.dataFind('description').find('text').text();
+        $lgzGameDesc = $winHintAvl.find('[resname=lgzGameDesc]');
+        $lgzGameDesc.text(gameDesc);
+
+
         $winHintAvl.css('display','block');
         for(i =0; i < hlist.length; i += 1) {
-            thisObj.add($winHintAvl, hlist[i]);
+            thisObj.add($winHintAvl, $(hlist[i]));
         }
             
     };
